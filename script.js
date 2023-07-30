b12028f88e07a6650e245c600abea10c3db535e9
Samidha Dervankar <137042559+Samidha399@users.noreply.github.com>
2023-07-30 17:02:06 +0530

    Eposide1
    All new Files added

---
+++ script.js
@@ -0,0 +1,9 @@
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hello World from JavaScript!";
+// heading.style.cssText = "font-size:25px;color:green;";
+// const root1 = document.getElementById("root");
+// root1.appendChild(heading);
+
+const heading = React.createElement("h1",{id:"h"},"Hello World from React Js.");
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
+root1.render(heading);
