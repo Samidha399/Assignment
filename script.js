1b89197194e6c90fc12d0223c2b915d6fdd64000
Samidha Dervankar <137042559+Samidha399@users.noreply.github.com>
2023-07-30 17:16:11 +0530

    Created nested react Element

--- script.js
+++ script.js
@@ -7,3 +7,10 @@
 const heading = React.createElement("h1",{id:"h"},"Hello World from React Js.");
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
 root1.render(heading);
+
+
+const parent = React.createElement("div",{id:'parent'},
+('div',{id:'child'},
+[('h1',{id:'tag1'},'This is from Child Tag.'),('h1',{id:'tag2'},'This is From Child Tag.')]));
+const main1 = ReactDOM.createRoot(document.getElementById("main"));
+main1.render(parent);
